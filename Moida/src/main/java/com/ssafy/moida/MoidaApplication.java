4d5e40fb0348e637ef5709bcf77ce3c63a2190c6
nakru03 <nakru03@gmail.com>
2020-05-11 10:47:56 +0900

    [초기화] 백엔드 초기커밋

---
+++ Moida/src/main/java/com/ssafy/moida/MoidaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.moida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoidaApplication.class, args);
+	}
+
+}
---
+++ Moida/src/test/java/com/ssafy/moida/MoidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.moida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
