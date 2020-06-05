089a599256e006221c7711448b65dccb8506b96b
문현식 <wnl668@naver.com>
2020-06-05 23:51:10 +0900

    [수정] music responseDTO 하나로 수정

--- Moida/src/main/java/com/ssafy/moida/service/music/MusicService.java
+++ Moida/src/main/java/com/ssafy/moida/service/music/MusicService.java
@@ -8,14 +8,14 @@ import com.ssafy.moida.web.dto.music.MusicFindByVideoidRequestDTO;
 import com.ssafy.moida.web.dto.music.MusicSaveRequestDTO;
 import com.ssafy.moida.web.dto.music.MusicSelcetMusicRequest;
 import com.ssafy.moida.web.dto.music.MusicFindByMoodRequestDTO;
-import com.ssafy.moida.web.dto.music.MusicFindByMoodResponseDTO;
+import com.ssafy.moida.web.dto.music.MusicResponseDTO;
 import com.ssafy.moida.web.dto.music.MusicFindByVideoResponse;
 
 public interface MusicService {
 	
 	public Long saveMusic(MusicSaveRequestDTO requestDTO) throws NumberFormatException, BaseException;
-	public Page<MusicFindByMoodResponseDTO> findByMood(MusicFindByMoodRequestDTO requestDto);
+	public Page<MusicResponseDTO> findByMood(MusicFindByMoodRequestDTO requestDto);
 	public Long selectMusic(MusicSelcetMusicRequest requestDto);
-	public MusicFindByVideoResponse findByVideoid(MusicFindByVideoidRequestDTO requestDTO);
+	public MusicResponseDTO findByVideoid(MusicFindByVideoidRequestDTO requestDTO);
 
 }
